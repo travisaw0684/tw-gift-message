001961e078c117576422ad01c47fb29e62bd3978
Travis Walker <traviswalker@Mac.attlocal.net>
2025-05-30 09:18:42 -0400

    Initial plugin setup with folder structure and empty TW_Gift_Message class

---
+++ tw-gift-message/tw-gift-message.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: WooCommerce TW Gift Message
+Description: Adds a gift message field to WooCommerce product pages, saving data through cart, order, and email flow.
+Version: 1.0.0
+Author: Travis Walker
+Text Domain: tw-gift-message
+*/
+
+// Exit if accessed directly
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+?>
